a78faffcf74361ed918bb49e943f4e6f1013ec4a
Jingying He <450883724@qq.com>
2025-02-28 21:27:33 +1100

    feat(feedback): add create a feedback API in the frontend

--- ui/src/feedback/api.ts
+++ ui/src/feedback/api.ts
@@ -5,6 +5,22 @@ export type Feedback = {
   text: string
 }
 
+export type Highlight = {
+  id: number;
+  feedbackId: number;
+  summary: string;
+  quote: string;
+}
+
+const createFeedback = gql`
+  mutation createFeedback($text: String!) {
+    createFeedback(text: $text) {
+      id
+      text
+    }
+  }
+`;
+
 const feedbacksDocument = gql`
   query feedbacks($page: Int!, $per_page: Int!) {
     feedbacks(page: $page, per_page: $per_page) {
@@ -23,3 +39,6 @@ export const feedbacksQuery = (page: number, per_page: number): Promise<Feedback
     page,
     per_page
   })
+
+export const createFeedbackQuery = (text: string): Promise<Feedback> =>
+  request('http://localhost:4000/graphql', createFeedback, { text });
