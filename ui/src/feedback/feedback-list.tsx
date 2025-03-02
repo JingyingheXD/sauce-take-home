3302da6beb358db5f8c787deadfb5fac323bd99e
Jingying He <450883724@qq.com>
2025-03-02 20:23:53 +1100

    feat(feedback list): add pagination

--- ui/src/feedback/feedback-list.tsx
+++ ui/src/feedback/feedback-list.tsx
@@ -4,26 +4,43 @@ import FeedbackCard from "./feedback-card.tsx";
 
 
 export default function FeedbackList() {
+  const PER_PAGE = 3;
+
   const [page, setPage] = useState(1);
+  const [totalCount, setTotalCount] = useState(0);
+
+  const totalPages = Math.max(Math.ceil(totalCount/PER_PAGE), 1);
+
   const [feedbacks, setFeedbacks] = useState<Feedback[]>([]);
   const [feedbackText, setFeedbackText] = useState('');
 
   useEffect(() => {
-    feedbacksQuery(page, 10).then((result) => setFeedbacks(result.feedbacks.values));
+    feedbacksQuery(page, PER_PAGE).then((result) => {
+      setFeedbacks(result.feedbacks.values);
+      setTotalCount(result.totalCount);
+    });
   }, [page]);
 
   const submitFeedback = async() => {
     try {
       await createFeedbackQuery(feedbackText);
-      setFeedbackText('');
     } catch(e) {
       console.error('insertion error: ', e);
     }
 
-    const feedbacksDocument = await feedbacksQuery(1, 100);
-    console.log('feedbacksDocument', feedbacksDocument);
-    
+    setFeedbackText('');
+    setPage(1);
+
+    const feedbacksDocument = await feedbacksQuery(page, PER_PAGE);
     setFeedbacks(feedbacksDocument.feedbacks.values);
+    setTotalCount(feedbacksDocument.totalCount);
+  };
+
+  const setPreviousPage = () => {
+    setPage(prevValue => Math.max(prevValue - 1, 1))
+  };
+  const setNextPage = () => {    
+    setPage(prevValue => Math.min(prevValue+1, totalPages))
   };
 
   return (
@@ -53,6 +70,29 @@ export default function FeedbackList() {
           <FeedbackCard feedback={feedback} key={feedback.id}></FeedbackCard>
         ))}
       </div>
+
+      <div className="flex flex-col gap-2 pt-4">
+        <div className="flex flex-row justify-end">
+          <button 
+            onClick={setPreviousPage}
+            disabled={page === 1}
+            className="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            Previous
+          </button>
+          <button
+            onClick={setNextPage}
+            disabled={page >= totalPages}
+            className="flex items-center justify-center px-3 h-8 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            Next
+          </button>
+        </div>
+        <div className="text-white flex items-center justify-end text-gray-500">
+          Page {page} of {totalPages}
+        </div>
+      </div>
+
     </div>
   );
 }
