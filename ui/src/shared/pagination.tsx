39c5dcf88a27aae32af012df7a7c4a4d4e0750dc
Jingying He <450883724@qq.com>
2025-03-02 21:12:22 +1100

    update(pagination): update button UI for disabled button status and more clear active button status

--- ui/src/shared/pagination.tsx
+++ ui/src/shared/pagination.tsx
@@ -3,18 +3,28 @@ export default function Pagination(props: PaginationProps) {
 
   return (
     <div className="flex flex-col gap-2 pt-4">
-      <div className="flex flex-row justify-end">
+      <div className="flex flex-row justify-end gap-4">
         <button 
           onClick={onPrevious}
           disabled={page === 1}
-          className="flex items-center justify-center px-3 h-8 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          className="flex items-center justify-center px-3 h-8 text-sm font-medium 
+                  text-gray-500 bg-white border border-gray-300 rounded-lg 
+                  hover:bg-gray-100 hover:text-gray-700 cursor-pointer
+                  dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 
+                  dark:hover:bg-gray-700 dark:hover:text-white 
+                    disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-gray-200"
         >
           Previous
         </button>
         <button
           onClick={onNext}
           disabled={page >= totalPages}
-          className="flex items-center justify-center px-3 h-8 ms-3 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          className="flex items-center justify-center px-3 h-8 text-sm font-medium 
+                  text-gray-500 bg-white border border-gray-300 rounded-lg 
+                  hover:bg-gray-100 hover:text-gray-700 cursor-pointer
+                  dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 
+                  dark:hover:bg-gray-700 dark:hover:text-white 
+                    disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-gray-200"
         >
           Next
         </button>
